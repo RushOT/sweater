d31fe34e0b31c771c01461c720146f331a6f6369
Akbar <akbartukhtamurotov@gmail.com>
2019-08-11 02:27:08 +0500

    Add relationships

--- src/main/java/uz/remnant/sweater/controller/MainController.java
+++ src/main/java/uz/remnant/sweater/controller/MainController.java
@@ -1,11 +1,13 @@
 package uz.remnant.sweater.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import uz.remnant.sweater.domain.Message;
+import uz.remnant.sweater.domain.User;
 import uz.remnant.sweater.repository.MessageRepository;
 import java.util.Map;
 
@@ -27,8 +29,13 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String, Object> model
+    ) {
+        Message message = new Message(text, tag, user);
         messageRepository.save(message);
 
         Iterable<Message> messages = messageRepository.findAll();
